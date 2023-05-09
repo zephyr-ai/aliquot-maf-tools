3599996ceb53b0188d1d85853236f8629a73a37b
Fahad Khan <fahadkhan2017@u.northwestern.edu>
2023-05-08 21:03:32 -0500

    Patch chromosome property to string-ify int-like values (#2)
    * Patch chromosome when the values are int-like
    
    * fix imports
    
    * remove print

--- aliquotmaf/subcommands/vcf_to_aliquot/runners/gdc_2_0_0_aliquot.py
+++ aliquotmaf/subcommands/vcf_to_aliquot/runners/gdc_2_0_0_aliquot.py
@@ -3,6 +3,7 @@ import urllib.parse
 
 import pysam
 from maflib.header import MafHeader, MafHeaderRecord
+from maflib.record import MafRecord
 from maflib.sort_order import BarcodesAndCoordinate
 from maflib.sorter import MafSorter
 from maflib.validation import ValidationStringency
@@ -278,6 +279,15 @@ class GDC_2_0_0_Aliquot(BaseRunner):
         tumor_sample_id = self.options["tumor_vcf_id"]
         normal_sample_id = self.options["normal_vcf_id"]
         is_tumor_only = self.options["tumor_only"]
+
+        # Patch some terrible typing issues on the MafRecord chromosome
+        class MonkeyMafRecord(MafRecord):
+            @property  # type: ignore
+            def chromosome(self):  # type: ignore
+                """Returns the chromosome name"""
+                return str(self["Chromosome"].value)
+
+        MafRecord.chromosome = MonkeyMafRecord.chromosome
 
         try:
             # Validate samples
